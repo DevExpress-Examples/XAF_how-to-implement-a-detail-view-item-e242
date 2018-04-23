0894af861a6968fba4a5bd99cae55620df50d926
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HowToImplementDetailViewItem.Module.Win/ClassIconDetailItem.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Drawing;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.ExpressApp.Win.Editors;
+using DevExpress.ExpressApp.Model;
+
+namespace HowToImplementDetailViewItem.Module.Win {
+
+    public interface IModelClassIcon : IModelViewItem { }
+
+	[ViewItemAttribute(typeof(IModelClassIcon))]
+	public class ClassIconDetailItem : ViewItem {
+        private IModelClassIcon Info;
+        public ClassIconDetailItem(IModelClassIcon info, Type classType)
+			: base(classType, info.Id) {
+            Info = info;
+		}
+		protected override object CreateControlCore() {
+			ResizablePictureBox imageControl = new ResizablePictureBox();
+			string imageName = ((IModelDetailView)Info.Parent.Parent).ImageName;
+			ImageInfo imageInfo = ImageLoader.Instance.GetLargeImageInfo(imageName);
+			if(imageInfo.IsEmpty) {
+				imageControl.Visible = false;
+			}
+			else {
+				Image image = imageInfo.Image;
+				imageControl.Image = image;
+				imageControl.Width = image.Width;
+				imageControl.Height = image.Height;
+			}
+			return imageControl;
+		}
+	}
+}
