1811002548c07bbb62462818a6b5bf918f27a02d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/HowToImplementDetailViewItem.Module/Module.vb
@@ -0,0 +1,13 @@
+Imports DevExpress.ExpressApp
+Imports System.Reflection
+
+
+Namespace HowToImplementDetailViewItem.Module
+    Public NotInheritable Partial Class HowToImplementDetailViewItemModule
+        Inherits ModuleBase
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
